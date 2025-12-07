d13241c68a462eb547ae0a67bbe211f1ebd42298
Farsa Suneer <farsansuneer@gmail.com>
2025-12-07 13:44:42 +0530

    program added

---
+++ p3.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class GuessNumber{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		int secret=27;
+		int guess;
+		int attempts=0;
+		System.out.println("Guess a number between 20 and 30");
+		do{
+			System.out.println("Guess Number: ");
+			guess=s.nextInt();
+			attempts++;
+			if(guess>secret){
+				System.out.println("Too High!");
+			}
+		        else if(guess<secret){
+				System.out.println("Too Low!");
+			}
+			else{
+				System.out.println("Your guess is correct");
+				System.out.println("Number of attempts: "+attempts);
+			}
+		}while(guess!=secret);
+	}
+}
+
