26b3a324fb072a59236ce5e08a535ea6805a404d
Farsa Suneer <farsansuneer@gmail.com>
2025-12-07 16:35:46 +0530

    program 7 added

---
+++ MaxThree.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class MaxNum{
+	int maxTwo(int a,int b){
+		if(a>b){
+			return a;
+		}
+		else{
+			return b;
+		}
+	}
+}
+class MaxThree{
+	public static void main(String args[]){
+		Scanner s=new Scanner(System.in);
+		MaxNum m=new MaxNum();
+		int ch;
+		do{
+			System.out.println("1.Max of number");
+			System.out.println("2.Exit");
+			System.out.println("Enter your choice: ");
+			ch=s.nextInt();
+			switch(ch){
+				case 1:
+				System.out.println("Enter a b c: ");
+				int a=s.nextInt();
+				int b=s.nextInt();
+				int c=s.nextInt();
+				int max=m.maxTwo(m.maxTwo(a,b),c);
+				System.out.println("Max= "+max);
+			case 2:
+				break;
+			default:
+				System.out.println("Wrong Choice");
+			}
+		}while(ch!=2);
+	}
+}
